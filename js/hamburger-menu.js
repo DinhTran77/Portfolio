e45cec41eea2cf72b4f77d36f31f7616a2eb087a
DinhTran77 <dinhtran77@hotmail.com>
2019-09-08 11:07:05 -0400

    slicknav menu

--- js/hamburger-menu.js
+++ js/hamburger-menu.js
@@ -3,11 +3,12 @@
             $('#menu').slicknav({
               label: "",
               brand: '<a href="#" id="text_logo">Dinh Tran</a>',
+              closeOnClick:'true',
               init: function() {
                 $bg = $('.slicknav_menu');
             },
             afterOpen: function(){
-                $bg.css({'background': 'transparent'});
+                $bg.css({'background': '#212529'});
             },
             afterClose: function(){
                 $bg.css({'background': 'transparent'});
