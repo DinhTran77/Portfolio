25ffe3acb81d422b2875537eba9700d830ed028d
DinhTran77 <dinhtran77@hotmail.com>
2019-09-09 10:37:17 -0400

    hamburger menu done by myself

--- js/hamburger-menu.js
+++ js/hamburger-menu.js
@@ -1,27 +1,26 @@
-
-		$(function() {
-            $('#menu').slicknav({
-              label: "",
-              brand: '<a href="#" id="text_logo">Dinh Tran</a>',
-              closeOnClick:'true',
-              init: function() {
-                $bg = $('.slicknav_menu');
-            },
-            afterOpen: function(){
-                $bg.css({'background': '#212529'});
-            },
-            afterClose: function(){
-                $bg.css({'background': 'transparent'});
-            }
-            });
-        });
-
-
+        
         function myFunction() {
-            var x = document.getElementById("myLinks");
-            if (x.style.display === "block") {
-              x.style.display = "none";
-            } else {
-              x.style.display = "block";
+            var myLinks = document.getElementById("myLinks");
+            var hamburger=document.getElementById("hamburger-icon");
+            var navigationLinks=document.getElementById('navigation-links');
+            var hamburger_font=document.getElementById('hamburger_font');
+
+            if (myLinks.style.display === "flex") {//when hamburger is closed
+              myLinks.style.display = "none";
+              navigationLinks.style.flexGrow = "1";
+              hamburger.style.flexGrow='1';
+              navigationLinks.style.flexBasis = "0";
+              navigationLinks.style.backgroundColor='transparent'
+
+              hamburger_font.style.paddingRight='25px';
+              // navigationLinks.style.justifyContent='none';
+
+            } else { //when hamburger is expanded
+              myLinks.style.display = "flex";
+              hamburger.style.flexGrow = "0";
+              navigationLinks.style.flexGrow = "1";
+              navigationLinks.style.justifyContent='flex-end';
+              navigationLinks.style.backgroundColor='grey'
+              // hamburger_font.style.padding='5px';
             }
           } 
