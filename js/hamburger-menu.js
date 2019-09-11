62ed95ea46945cbfb36aeb41654246f7c9b96b9a
DinhTran77 <dinhtran77@hotmail.com>
2019-09-10 20:09:56 -0400

    nav-bar working

--- js/hamburger-menu.js
+++ js/hamburger-menu.js
@@ -1,26 +1,16 @@
+
+	// $(function(){
+	// 	$('#menu').slicknav(
+  //     {
+  //       label: "",
+  //       brand: '<a href="#" id="logo_text">Dinh Tran</a>'
+  //     }
+
+  //   );
+	// });
         
-        function myFunction() {
-            var myLinks = document.getElementById("myLinks");
-            var hamburger=document.getElementById("hamburger-icon");
-            var navigationLinks=document.getElementById('navigation-links');
-            var hamburger_font=document.getElementById('hamburger_font');
+        
+    
 
-            if (myLinks.style.display === "flex") {//when hamburger is closed
-              myLinks.style.display = "none";
-              navigationLinks.style.flexGrow = "1";
-              hamburger.style.flexGrow='1';
-              navigationLinks.style.flexBasis = "0";
-              navigationLinks.style.backgroundColor='transparent'
 
-              hamburger_font.style.paddingRight='25px';
-              // navigationLinks.style.justifyContent='none';
-
-            } else { //when hamburger is expanded
-              myLinks.style.display = "flex";
-              hamburger.style.flexGrow = "0";
-              navigationLinks.style.flexGrow = "1";
-              navigationLinks.style.justifyContent='flex-end';
-              navigationLinks.style.backgroundColor='grey'
-              // hamburger_font.style.padding='5px';
-            }
-          } 
+  
