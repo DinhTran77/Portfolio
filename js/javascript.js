f92ea69a3d5ec50042bb67f0eeb3e8137bafacdd
DinhTran77 <dinhtran77@hotmail.com>
2019-09-18 18:23:14 -0400

    Old hamburger

--- js/javascript.js
+++ js/javascript.js
@@ -1,4 +1,15 @@
-
+// Initialize SlickNav (Hamburger menu plugin)
+	$(function(){
+		$('#menu').slicknav(
+			{
+			'label' : ''
+			}
+		);
+  });
+  
+// initialize AOS (Automate on Scroll Plugin
+	
+  AOS.init();	
 
 //Smooth scroll and offset for fixed navbar
 $('a[href^="#"]').on('click', function(event) {
@@ -12,4 +23,34 @@ $('a[href^="#"]').on('click', function(event) {
       }, 500);
   }
 });
+
+
+// Type writter animation
+var i = 0;
+var txt = 'Clean code, so help me God.'; /* The text */
+
+
+ /* The speed/duration of the effect in milliseconds */
+let j = 0;
+var speed = 60;
+
+function typeWriter() 
+    {
+
+        if(j<txt.length)
+            {
+            document.getElementById("hero_text").innerHTML += txt.charAt(j);
+            j++;
+         
+            setTimeout(typeWriter, speed);
+            }
+            if(txt.charAt(j)==='s')
+            {
+              document.getElementById("hero_text").innerHTML += "<br/>";
+            }
+    }
+
+    setTimeout(function() {
+      typeWriter();
+    }, 800);
 
