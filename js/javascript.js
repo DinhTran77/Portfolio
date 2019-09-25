0c3b91cc9afec45863c9fd161f9433eb290a94c1
DinhTran77 <dinhtran77@hotmail.com>
2019-09-25 13:21:16 -0400

    home page done

--- js/javascript.js
+++ js/javascript.js
@@ -12,17 +12,17 @@
   AOS.init();	
 
 //Smooth scroll and offset for fixed navbar
-$('a[href^="#"]').on('click', function(event) {
+// $('a[href^="#"]').on('click', function(event) {
 
-  var navBarHeight=$('#topnav_area').height();
-  var target = $(this.getAttribute('href'));
-  if( target.length ) {
-      event.preventDefault();
-      $('html, body').stop().animate({
-          scrollTop: target.offset().top-navBarHeight
-      }, 500);
-  }
-});
+//   var navBarHeight=$('#topnav_area').height();
+//   var target = $(this.getAttribute('href'));
+//   if( target.length ) {
+//       event.preventDefault();
+//       $('html, body').stop().animate({
+//           scrollTop: target.offset().top-navBarHeight
+//       }, 500);
+//   }
+// });
 
 
 // Type writter animation
