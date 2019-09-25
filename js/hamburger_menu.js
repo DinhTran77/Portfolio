28ba26a121c37c6a57edc26a31d3e88986dd71c4
DinhTran77 <dinhtran77@hotmail.com>
2019-09-24 22:00:05 -0400

    new hamburger works

--- js/hamburger_menu.js
+++ js/hamburger_menu.js
@@ -10,16 +10,31 @@ function toggleMenu()
                         $("#menu").removeClass('slide-out');  
                         $("#menu").addClass('slide-in');
 
-                        $('#hamburger-icon i').removeClass('fas fa-bars');
-                        $('#hamburger-icon i').addClass('fas fa-times');
+                        $("#line1").removeClass('out_top_line');
+                        $("#line3").removeClass('out_bottom_line');
+                        $("#line2").removeClass('out_middle_line');
+
+                        $("#line1").addClass('in_top_line');
+                        $("#line3").addClass('in_bottom_line');
+                        $("#line2").addClass('in_middle_line');
+                        // $('#hamburger-icon i').removeClass('fas fa-bars');
+                        // $('#hamburger-icon i').addClass('fas fa-times');
                         }
                 else //Menu is closed
                         {
                         $("#menu").removeClass('slide-in');   
                         $("#menu").addClass('slide-out');  
 
-                        $('#hamburger-icon i').removeClass('fas fa-times');
-                        $('#hamburger-icon i').addClass('fas fa-bars');
+                        $("#line1").removeClass('in_top_line');
+                        $("#line3").removeClass('in_bottom_line');
+                        $("#line2").removeClass('in_middle_line');
+                        
+                        $("#line1").addClass('out_top_line');
+                        $("#line3").addClass('out_bottom_line');
+                        $("#line2").addClass('out_middle_line');
+
+                        // $('#hamburger-icon i').removeClass('fas fa-times');
+                        // $('#hamburger-icon i').addClass('fas fa-bars');
                         }     
         }
 
