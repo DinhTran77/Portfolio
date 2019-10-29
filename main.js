e8854dce4ebc324ada8d2f22b89af9b10ae04c68
DinhTran77 <dinhtran77@hotmail.com>
2019-10-29 11:56:04 -0400

    david and normal css working

--- main.js
+++ main.js
@@ -5,46 +5,46 @@ AOS.init();
 //Form submission
 
 $("form.ajax").on("submit", function() {
-    let url = $(this).attr("action");
-    let type = $(this).attr("method");
+        let url = $(this).attr("action");
+        let type = $(this).attr("method");
 
-    let name = $("#name").val();
-    let email = $("#email").val();
-    let message = $("#comment").val();
-    let data = {
-        name: name,
-        email: email,
-        message: message
-    };
+        let name = $("#name").val();
+        let email = $("#email").val();
+        let message = $("#comment").val();
+        let data = {
+                name: name,
+                email: email,
+                message: message
+        };
 
-    $.ajax({
-        url: url,
-        type: type,
-        data: data,
-        success: function(response) {
-            $("#sucess_message").html(response);
-            $("#sucess_message").css("display", "inline-block");
-        }
-    });
-    return false;
+        $.ajax({
+                url: url,
+                type: type,
+                data: data,
+                success: function(response) {
+                        $("#sucess_message").html(response);
+                        $("#sucess_message").css("display", "inline-block");
+                }
+        });
+        return false;
 });
 
 //*************************Type writter animation***********************
 
 const text = "Clean & maintainable front-end code";
 const speed = 60;
-const heroText = document.getElementById("hero_text");
-const heroGreeting = document.getElementById("hero_greeting");
+const heroText = document.getElementsByClassName("hero_text");
+const heroGreeting = document.getElementsByClassName("hero_greeting");
 
 let l = 0;
 function typeWriter() {
-    if (l < text.length) {
-        heroText.innerHTML += text.charAt(l);
-        l++;
-        setTimeout(typeWriter, speed);
-    }
+        if (l < text.length) {
+                heroText[0].innerHTML += text.charAt(l);
+                l++;
+                setTimeout(typeWriter, speed);
+        }
 }
 
 setTimeout(function() {
-    typeWriter();
+        typeWriter();
 }, 800);
