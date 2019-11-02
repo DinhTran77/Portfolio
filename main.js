f2173caa663416bd931605a3a6acac73a2dd7ba0
DinhTran77 <dinhtran77@hotmail.com>
2019-11-01 20:26:38 -0400

    About page SCSS cleaned up

--- main.js
+++ main.js
@@ -1,45 +1,48 @@
 //************initialize AOS (Automate on Scroll) Plugin
 AOS.init();
 
-// Fade-in Nav bar on scroll
-// $(document).ready(function() {
-//         $(window).scroll(function() {
-//                 if ($(this).scrollTop() > 100) {
-//                         // $("#nav-bar").css("background-color", "black");
-//                         $("#nav-bar").removeClass("nav-fade-out-desktop");
-//                         $("#nav-bar").addClass("nav-fade-in-desktop");
-//                 } else {
-//                         $("#nav-bar").removeClass("nav-fade-in-desktop");
-//                         $("#nav-bar").addClass("nav-fade-out-desktop");
-//                         // $("#nav-bar").css("background-color", "transparent");
-//                 }
-//         });
-// });
+//script to fill skills templates
+$(function() {
+    $.getJSON("skills.json", function(data) {
+        var template = $("#skills_template").html();
+        var html = Mustache.to_html(template, data);
+        $("#skill").html(html);
+    });
+});
+
+//script to fill portfolio templates
+$(function() {
+    $.getJSON("projects.json", function(data) {
+        var template = $("#portfolio_template").html();
+        var html = Mustache.to_html(template, data);
+        $("#project").html(html);
+    });
+});
 
 //Contact Form submission
 $("form.ajax").on("submit", function() {
-        let url = $(this).attr("action");
-        let type = $(this).attr("method");
+    let url = $(this).attr("action");
+    let type = $(this).attr("method");
 
-        let name = $("#name").val();
-        let email = $("#email").val();
-        let message = $("#comment").val();
-        let data = {
-                name: name,
-                email: email,
-                message: message
-        };
+    let name = $("#name").val();
+    let email = $("#email").val();
+    let message = $("#comment").val();
+    let data = {
+        name: name,
+        email: email,
+        message: message
+    };
 
-        $.ajax({
-                url: url,
-                type: type,
-                data: data,
-                success: function(response) {
-                        $("#sucess_message").html(response);
-                        $("#sucess_message").css("display", "inline-block");
-                }
-        });
-        return false;
+    $.ajax({
+        url: url,
+        type: type,
+        data: data,
+        success: function(response) {
+            $("#sucess_message").html(response);
+            $("#sucess_message").css("display", "inline-block");
+        }
+    });
+    return false;
 });
 
 //*************************Type writter animation***********************
@@ -51,13 +54,13 @@ const heroGreeting = document.getElementsByClassName("hero_greeting");
 
 let l = 0;
 function typeWriter() {
-        if (l < text.length) {
-                heroText[0].innerHTML += text.charAt(l);
-                l++;
-                setTimeout(typeWriter, speed);
-        }
+    if (l < text.length) {
+        heroText[0].innerHTML += text.charAt(l);
+        l++;
+        setTimeout(typeWriter, speed);
+    }
 }
 
 setTimeout(function() {
-        typeWriter();
+    typeWriter();
 }, 800);
